891de7e2bcd621fabd5e372bf4d1edd58d34f3bf
menigotc <corentin.menigot@etu.univ-grenobles-alpes.fr>
2024-03-29 11:30:27 +0100

    Fichier gitignore modifié

--- src/tp4/dalton/Dalton.java
+++ src/tp4/dalton/Dalton.java
@@ -1,6 +1,8 @@
 package tp4.dalton;
 
-public class Dalton {
+import java.util.Comparator;
+
+public class Dalton implements Comparable<Dalton> {
     //attributs
     private String nom;
     private Taille taille;
@@ -14,4 +16,15 @@ public class Dalton {
     //methodes
     public String getNom () { return this.nom;}
     public Taille getTaille () { return this.taille;}
+
+    @Override
+    public int compareTo(Dalton dalton) {
+        if (this.taille.compareTo(dalton.taille)<0 || (this.taille == dalton.taille && this.nom.compareTo(dalton.nom)<0)) {
+            return -1;
+        } else if (this.nom.equals(dalton.nom) && this.taille == dalton.taille) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
