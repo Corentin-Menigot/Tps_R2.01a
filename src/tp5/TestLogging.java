caafcc8b69c24257c3804e931eaafb67acf273d8
menigotc <corentin.menigot@etu.univ-grenobles-alpes.fr>
2024-04-02 17:10:37 +0200

    Fichier gitignore modifié

--- src/tp5/TestLogging.java
+++ src/tp5/TestLogging.java
@@ -1,15 +1,26 @@
 package tp5;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class TestLogging {
     // Récupération du logger
     private static Logger LOGGER = Logger.getLogger(TestLogging.class.getPackageName());
     // Configuration du logger
-    static {
-        System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$s]  %4$-10s | (%3$s) %2$-15s | %5$s\n");
-        LOGGER.setLevel(Level.WARNING);
+    // Récupérarion du gestionnaire de logs.
+    private static final LogManager logManager = LogManager.getLogManager();
+    // Configuration du logger
+    // EditConfiguration > Modify options > add VM options :
+    // -Djava.util.logging.config.file=conf/debug-logging.properties
+    static{
+        try {
+            logManager.readConfiguration( new FileInputStream("conf/debuglogging.properties") );
+        } catch ( IOException exception ) {
+            LOGGER.log( Level.SEVERE, "Cannot read configuration file", exception );
+        }
     }
 
     public static void main(String[] args) {
