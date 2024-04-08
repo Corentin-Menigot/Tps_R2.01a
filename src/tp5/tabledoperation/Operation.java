999171b7b864f0afd6b22442724880c799520819
menigotc <corentin.menigot@etu.univ-grenobles-alpes.fr>
2024-04-08 10:54:24 +0200

    Fichier gitignore modifié

--- src/tp5/tabledoperation/Operation.java
+++ src/tp5/tabledoperation/Operation.java
@@ -1,4 +1,28 @@
 package tp5.tabledoperation;
 
-public class Operation {
+public abstract class Operation {
+    //attributs de la classe
+    private double terme1;
+    private double terme2;
+    private Double reponseUtilisateur;
+
+    //constructer de la classe
+    public Operation (double terme1, double terme2) {
+
+    }
+
+    //méthodes et fpnctions de la classe
+
+    public void setReponseUtilisateur(double reponse) {
+        this.reponseUtilisateur = reponse;
+    }
+    public boolean existReponseUtilisateur () {
+        return this.reponseUtilisateur != null;
+    }
+    public boolean isReponseJuste() {
+        return reponseUtilisateur == calculResultat();
+    }
+    public abstract double calculResultat();
+    public double getTerme1 () { return this.terme1;}
+    public double getTerme2 () { return this.terme2;}
 }
--- src/tp5/tabledoperation/Soustraction.java
+++ src/tp5/tabledoperation/Soustraction.java
@@ -1,4 +1,18 @@
 package tp5.tabledoperation;
 
-public class Soustraction {
+public class Soustraction extends Operation {
+    //constructeur
+    public Soustraction(double terme1, double terme2) {
+        super(terme1, terme2);
+    }
+
+    //méthodes
+    @Override
+    public double calculResultat() {
+        return super.getTerme1() - super.getTerme2();
+    }
+    @Override
+    public String toString() {
+        return super.getTerme1()+"-"+super.getTerme2();
+    }
 }
