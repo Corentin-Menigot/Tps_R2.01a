18110e89c4f1138298903e42c087286fcab9d1c0
menigotc <corentin.menigot@etu.univ-grenobles-alpes.fr>
2024-04-08 10:55:40 +0200

    Fichier gitignore modifié

--- src/tp5/tabledoperation/Addition.java
+++ src/tp5/tabledoperation/Addition.java
@@ -1,4 +1,18 @@
 package tp5.tabledoperation;
 
-public class Addition {
+public class Addition extends Operation {
+    //constructeur
+    public Addition(double terme1, double terme2) {
+        super(terme1, terme2);
+    }
+
+    //méthodes
+    @Override
+    public double calculResultat() {
+        return super.getTerme1() + super.getTerme2();
+    }
+    @Override
+    public String toString() {
+        return super.getTerme1()+"+"+super.getTerme2();
+    }
 }
--- src/tp5/tabledoperation/Multiplication.java
+++ src/tp5/tabledoperation/Multiplication.java
@@ -1,4 +1,18 @@
 package tp5.tabledoperation;
 
-public class Multiplication {
+public class Multiplication extends Operation {
+    //constructeur
+    public Multiplication(double terme1, double terme2) {
+        super(terme1, terme2);
+    }
+
+    //méthodes
+    @Override
+    public double calculResultat() {
+        return super.getTerme1() * super.getTerme2();
+    }
+    @Override
+    public String toString() {
+        return super.getTerme1()+"*"+super.getTerme2();
+    }
 }
