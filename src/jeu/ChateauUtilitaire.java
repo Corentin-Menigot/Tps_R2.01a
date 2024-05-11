84655196b4059121768d3c229fc3ab0709e5cd46
corentin <corentin@menigot.fr>
2024-05-11 16:28:19 +0200

    Ajout des commentaires et de la javadoc pour la classe ChateauUtilitaire

--- src/jeu/ChateauUtilitaire.java
+++ src/jeu/ChateauUtilitaire.java
@@ -1,9 +1,6 @@
 package jeu;
 
-
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 
 /**
  *
@@ -11,14 +8,33 @@ import java.util.Iterator;
  */
 public class ChateauUtilitaire {
 
-    
-    ///////////////////////
     // METHODES D'AFFICHAGE
+
+    /**
+     *
+     * Affiche les informations de guerriers d'une liste de guerriers donnée,
+     * un guerrier par ligne
+     *
+     * @param listeGuerriers -> la liste des guerriers à afficher
+     */
+    public static void printListeGuerriers(ArrayList<Guerrier> listeGuerriers) {
+        for (int i = 0; i < listeGuerriers.size(); i++) {
+            //appelle la méthode printUnite pour afficher un guerrier
+            GuerrierUtilitaire.printUnite(listeGuerriers.get(i));
+        }
+    }
+
+    /**
+     *
+     * Affiche les informations de guerriers d'un chateau donné,
+     * un guerrier par ligne
+     *
+     * @param chateau -> le chateau duquel on affiche les guerriers novices
+     */
   	public static void printChateauGuerriers(Chateau chateau) {
         System.out.println("Guerriers du chateau: ");
-        ArrayList<Guerrier> guerriers = chateau.getGuerrierNovice();
-        for (Iterator<Guerrier> it = guerriers.iterator(); it.hasNext();) {
-            GuerrierUtilitaire.printUnite(it.next());
-        }
+        ArrayList<Guerrier> listeGuerriers = chateau.getGuerrierNovice();
+        //appelle la méthode printListeGuerrier pour afficher la liste des guerriers réupérés du chateau
+        printListeGuerriers(listeGuerriers);
     }
 }
