e3af427655c5b9db0c3bb6e4c9e726125616b916
corentin <corentin@menigot.fr>
2024-05-12 18:28:11 +0200

    Ajout de quelques tests unitaires dans la classe ChateauTest

--- test/jeu/ChateauTest.java
+++ test/jeu/ChateauTest.java
@@ -23,13 +23,34 @@ class ChateauTest {
 
     @Test
     void getCouleur() {
+        //
+        Chateau chateau1 = new Chateau(Couleur.Rouge);
+        assertEquals(chateau1.getCouleur(), Couleur.Rouge);
+
+        //
+        Chateau chateau2 = new Chateau(Couleur.Bleu);
+        assertEquals(chateau2.getCouleur(), Couleur.Bleu);
     }
 
     @Test
     void estBleu() {
+        //
+        Chateau chateau1 = new Chateau(Couleur.Bleu);
+        assertTrue(chateau1.estBleu());
+
+        //
+        Chateau chateau2 = new Chateau(Couleur.Rouge);
+        assertFalse(chateau2.estBleu());
     }
 
     @Test
     void estRouge() {
+        //
+        Chateau chateau1 = new Chateau(Couleur.Rouge);
+        assertTrue(chateau1.estRouge());
+
+        //
+        Chateau chateau2 = new Chateau(Couleur.Bleu);
+        assertFalse(chateau2.estRouge());
     }
 }
